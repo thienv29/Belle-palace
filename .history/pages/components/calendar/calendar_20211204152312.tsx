79b41bf3f60e9dd755d740ab4588ef3667545dd3
thienv29 <thienv29@gmail.com>
2021-12-04 15:23:23 +0700

    first commit

--- .history/pages/components/calendar/calendar_20211204152312.tsx
+++ .history/pages/components/calendar/calendar_20211204152312.tsx
@@ -3,6 +3,7 @@ import { Container, Row, Col } from 'react-bootstrap'
 import style from './calendar.module.css'
 
 const Calendar = (props:any) => {
+
     const result = props.data.result
     return (
         <div className={style.container}>
